c0fd130515067a7f2e23f24a77c0959ad360c6e2
Tom Smith <thomas.smith5@nuh.nhs.uk>
2021-10-16 00:53:21 +0100

    classify robson group 8

--- R/tgcs_classify.R
+++ R/tgcs_classify.R
@@ -5,6 +5,14 @@ tgcs_classify <- function(.data){
     msg = "tgcs_classify: .data must be a data.frame"
   )
 
-tgcs_validate_dataframe(.data)
+  tgcs_validate_dataframe(.data)
+
+  # prepare a result dataframe with a Robson_Classification column
+  result <- .data %>% dplyr::mutate(Robson_Classification = "not yet classified")
+
+  # classify robson group 8
+  result[result$Number_Of_Fetuses == "Multiple","Robson_Classification"] <- "8"
+
+  return(result)
 
 }
--- tests/testthat/test-tgcs_classify.R
+++ tests/testthat/test-tgcs_classify.R
@@ -1,12 +1,19 @@
 library(mockery)
 
+data <- data.frame(
+    Parity = 1,
+    Previous_CS = 1,
+    Onset_Of_Labour = 1,
+    Number_Of_Fetuses = 1,
+    Gestational_Age = 1,
+    Fetal_Lie_And_Presentation = 1
+  )
+
 test_that("it throws an error if .data is not a data.frame", {
   expect_error(tgcs_classify("not a data frame"), "tgcs_classify: .data must be a data.frame")
 })
 
 test_that("it calls tgcs_validate_dataframe()", {
-
-  data <- data.frame()
 
   m <- mock("tgcs_validate_dataframe")
   stub(tgcs_classify, "tgcs_validate_dataframe", m)
@@ -16,4 +23,24 @@ test_that("it calls tgcs_validate_dataframe()", {
   expect_called(m, 1)
   expect_args(m, 1, data)
 
+})
+
+test_that("it returns a data frame with a column for classification", {
+  o <- tgcs_classify(data)
+  expect_equal("Robson_Classification" %in% colnames(o), TRUE)
+})
+
+test_that("it correctly classifies robson group 8", {
+
+  data <- data.frame(
+    Parity = NA,
+    Previous_CS = NA,
+    Onset_Of_Labour = NA,
+    Number_Of_Fetuses = rep(c("Multiple", "Single"), 2),
+    Gestational_Age = NA,
+    Fetal_Lie_And_Presentation = NA
+  )
+
+  o <- tgcs_classify(data)
+  expect_equal(nrow(o[o$Robson_Classification == "8", ]), 2) # 2 rows classified as robson 8
 })
