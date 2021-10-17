b7286f20d053391dd76b95af65cedca18cb108d0
Tom Smith <thomas.smith5@nuh.nhs.uk>
2021-10-17 20:11:48 +0100

    improve tests confirm existing classifications are... not modified

--- tests/testthat/test-tgcs_classify_rg6.R
+++ tests/testthat/test-tgcs_classify_rg6.R
@@ -15,3 +15,20 @@ test_that("it correctly classifies robson group 6", {
   expect_equal(nrow(o[o$Robson_Classification == "6", ]), 1) # 1 row classified as robson 7
   expect_equal(nrow(o[o$Robson_Classification == "Unclassifiable", ]), 6) # 6 rows unclassifiable
 })
+
+test_that("it does not alter existing classifications", {
+
+  data <- data.frame(
+    Parity = NA,
+    Previous_CS = NA,
+    Onset_Of_Labour = NA,
+    Number_Of_Fetuses = NA,
+    Gestational_Age = NA,
+    Fetal_Lie_And_Presentation = NA,
+    Robson_Classification = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Unclassifiable")
+  )
+
+  o <- tgcs_classify_rg10(data)
+  expect_equal(nrow(o), 11) # 11 rows in total
+  expect_equal(o$Robson_Classification, c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Unclassifiable"))
+})
