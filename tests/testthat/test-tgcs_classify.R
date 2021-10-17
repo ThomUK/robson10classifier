22dca51c56ba8fd18b7e9aa44880c017a1c16656
Tom Smith <thomas.smith5@nuh.nhs.uk>
2021-10-17 20:29:14 +0100

    add test to cover every variable combination

--- tests/testthat/test-tgcs_classify.R
+++ tests/testthat/test-tgcs_classify.R
@@ -29,3 +29,31 @@ test_that("it returns a data frame with a column for classification", {
   o <- tgcs_classify(data)
   expect_equal("Robson_Classification" %in% colnames(o), TRUE)
 })
+
+test_that("it can classify every combination of the 6 obstetric variables", {
+
+  # define all possible levels for each obstetric variable
+  Parity <- c("Nullipara", "Multipara", NA)
+  Previous_CS <- c("None", "One or more", NA)
+  Onset_Of_Labour <- c("Spontaneous", "Induced", "Pre-labour CS", NA)
+  Number_Of_Fetuses <- c("Multiple", "Single", NA)
+  Gestational_Age <- c("Term", "Preterm", NA)
+  Fetal_Lie_And_Presentation <- c("Cephalic", "Breech", "Transverse or Oblique lie", NA)
+
+  #combine them to give a data frame of all 1296 possible combinations
+  data <- tidyr::crossing(Parity, Previous_CS, Onset_Of_Labour, Number_Of_Fetuses, Gestational_Age, Fetal_Lie_And_Presentation)
+
+  o <- tgcs_classify(data)
+  expect_equal(nrow(o[o$Robson_Classification == "Unclassifiable", ]), 647)
+  expect_equal(nrow(o[o$Robson_Classification == "1", ]), 2)
+  expect_equal(nrow(o[o$Robson_Classification == "2", ]), 4)
+  expect_equal(nrow(o[o$Robson_Classification == "3", ]), 1)
+  expect_equal(nrow(o[o$Robson_Classification == "4", ]), 2)
+  expect_equal(nrow(o[o$Robson_Classification == "5", ]), 4)
+  expect_equal(nrow(o[o$Robson_Classification == "6", ]), 36)
+  expect_equal(nrow(o[o$Robson_Classification == "7", ]), 36)
+  expect_equal(nrow(o[o$Robson_Classification == "8", ]), 432)
+  expect_equal(nrow(o[o$Robson_Classification == "9", ]), 108)
+  expect_equal(nrow(o[o$Robson_Classification == "10", ]), 24)
+
+})
