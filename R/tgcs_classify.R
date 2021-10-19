8e0a649e301b18d6d149acc4453b0125f039535c
Tom Smith <thomas.smith5@nuh.nhs.uk>
2021-10-19 01:26:26 +0100

    CMD check fix

--- R/tgcs_classify.R
+++ R/tgcs_classify.R
@@ -25,7 +25,7 @@ tgcs_classify <- function(.data){
 
   # prepare a result dataframe with a Robson_Classification column
   result <- .data %>%
-    dplyr::mutate(Robson_Classification = "not yet classified", .before = Parity)
+    dplyr::mutate(Robson_Classification = "not yet classified", .before = .data$Parity)
 
   result <- result %>%
     tgcs_classify_rg8() %>%
